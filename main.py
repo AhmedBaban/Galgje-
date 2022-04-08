cc30e3252b7c7696627176bac5ad62ac86fe6d38
Noah Onder de Linden <95483517+Rackmoer@users.noreply.github.com>
2022-04-08 13:44:14 +0000

    welkom scherm en list toegevoegd

---
+++ main.py
@@ -0,0 +1,6 @@
+#data
+list = ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier', 'fotografie', 'waardebepaling', 'specialiteit', 'verzekering', 'universiteit', 'heesterperk']
+
+#code
+print("Hallo, welkom bij galgje")
+print("Het doel van het spel is om een woord raden door letters te kiezen die in het woord zitten. Je krijgt te weten hoeveel letters er in het woord zitten. Je hebt tien beurten, bij elke fout gaat er één beurt van af.")
